8d2f73fc76614e2f46615fc57d00acecd0d34bb4
Arron-Reed <arron.reed@chasacademy.se>
2023-05-16 01:03:59 +0200

    Cleaned up code

--- src/components/Search/SearchComponent.jsx
+++ src/components/Search/SearchComponent.jsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
 import "./searchComponent.css";
 import { dayOfWeek } from "../../dayOfWeek";
-import { useRef } from 'react';
+import { getMonth } from "../../getMonth";
+import { useRef } from "react";
 
 export const Search = () => {
-  
   const [forecast, setForecast] = useState();
   const [status, setStatus] = useState("");
   const [city, setCity] = useState("");
-  const [celcius, setCelcius] = useState("celcius");
+  const [celcius, setCelcius] = useState("true");
 
   const getWeather = async () => {
     try {
       if (city !== "") {
         const apiKey = import.meta.env.VITE_API_KEY_WeatherAPI;
-      const URL =
-        "https://api.weatherapi.com/v1/forecast.json?key=" +
-        apiKey +
-        "&q=" +
-        city +
-        "&days=5" +
-        "&aqi=no" +
-        "&alerts=no";
+        const URL =
+          "https://api.weatherapi.com/v1/forecast.json?key=" +
+          apiKey +
+          "&q=" +
+          city +
+          "&days=5" +
+          "&aqi=no" +
+          "&alerts=no";
 
-      const response = await fetch(URL);
-      const result = await response.json();
-      
-      setForecast(result);
+        const response = await fetch(URL);
+        const result = await response.json();
 
+        setForecast(result);
       }
-      
     } catch (error) {
       console.log(error);
     }
@@ -42,37 +40,38 @@ export const Search = () => {
       setStatus("Loading...");
     }
 
-    navigator.geolocation.getCurrentPosition( (position) => {
-      setStatus(""); setCity(position.coords.latitude + "," + position.coords.longitude);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setStatus("");
+        setCity(position.coords.latitude + "," + position.coords.longitude);
       },
 
       () => {
         setStatus("Unable to retrieve your position");
       }
-      
     );
   };
 
-
   useEffect(() => {
-  if (city == "")
-    getLocation();
+    if (city == "") getLocation();
     getWeather();
   }, [city]);
 
-
   const inputRef = useRef(null);
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setCity(inputRef.current.value);
   };
 
+  const handleToggle = () => {
+    setCelcius(!celcius);
+  };
+  
+
   return (
     <div className="search-container">
       <div className="search-box">
-
         <form onSubmit={(event) => handleSubmit(event)} className="search-form">
           <input
             type="text"
@@ -81,6 +80,20 @@ export const Search = () => {
           ></input>
           <button type="submit">Search</button>
         </form>
+
+        <div className="addOn">
+          <div>
+            <button className="currentLocation" onClick={getLocation}>
+              Current Location
+            </button>
+          </div>
+
+          <div>
+            <button className="celciusFarenheit" onClick={handleToggle}>
+              {celcius ? "°F" : "°C"}
+              </button>
+          </div>
+        </div>
       </div>
 
       {forecast && (
@@ -91,21 +104,40 @@ export const Search = () => {
             </h3>
 
             <div className="header">
-              
               <div>
-                <img src={forecast.current.condition.icon} className="weather-image"></img>
+                <img
+                  src={forecast.current.condition.icon}
+                  className="weather-image"
+                ></img>
               </div>
 
               <div className="right-header">
-                <p className="temp">{forecast.current.temp_c}°C</p>
-                <p className="feelsLike">
-                  Feels like {Math.round(forecast.current.feelslike_c)}°C
-                </p>
+                {celcius && (
+                  <p className="temp">
+                    {Math.round(forecast.current.temp_c)}°C
+                  </p>
+                )}
+                {!celcius && (
+                  <p className="temp">
+                    {Math.round((forecast.current.temp_c * 9) / 5) + 32}°F
+                  </p>
+                )}
+
+                {celcius && (
+                  <p className="feelsLike">
+                    Feels like {Math.round(forecast.current.feelslike_c)}°C
+                  </p>
+                )}
+                {!celcius && (
+                  <p className="feelsLike">
+                    Feels like{" "}
+                    {Math.round((forecast.current.feelslike_c * 9) / 5) + 32}°F
+                  </p>
+                )}
               </div>
             </div>
 
             <div className="info">
-
               <div className="left">
                 <p>Wind</p>
                 <p>Humidity</p>
@@ -114,125 +146,141 @@ export const Search = () => {
               </div>
 
               <div className="right">
-                <p>{Math.round(forecast.current.wind_kph *0.277778)} m/s - {forecast.current.wind_dir}</p>
+                <p>
+                  {Math.round(forecast.current.wind_kph * 0.277778)} m/s -{" "}
+                  {forecast.current.wind_dir}
+                </p>
                 <p>{forecast.current.humidity}%</p>
-                <p>{forecast.forecast.forecastday[0].astro.sunrise.toLowerCase()}</p>
-                <p>{forecast.forecast.forecastday[0].astro.sunset.toLowerCase()}</p>
+                <p>
+                  {forecast.forecast.forecastday[0].astro.sunrise.toLowerCase()}
+                </p>
+                <p>
+                  {forecast.forecast.forecastday[0].astro.sunset.toLowerCase()}
+                </p>
               </div>
-
             </div>
           </div>
 
           <div className="forecast">
-            
-            <div className="forecast-box">
-              <p className="weekDay"> { dayOfWeek( new Date(forecast.forecast.forecastday[0].date_epoch * 1000).getDay() ) } </p>
-              <img src={forecast.forecast.forecastday[0].day.condition.icon} className="forecast-image"></img>  
-              <div className="forecast-temp">
-                <p className="ft-left">{Math.round(forecast.forecast.forecastday[0].day.mintemp_c)}°</p> 
-                <p  className="ft-right">{Math.round(forecast.forecast.forecastday[0].day.maxtemp_c)}°</p>
-              </div>
-            </div>
+            {[0, 1, 2, 3, 4].map((i) => (
+              <div className="forecast-box" key={i}>
+                <p className="weekDay">
+                  {" "}
+                  {dayOfWeek(
+                    new Date(
+                      forecast.forecast.forecastday[i].date_epoch * 1000
+                    ).getDay()
+                  )}{" "}
+                </p>
+                <img
+                  src={forecast.forecast.forecastday[i].day.condition.icon}
+                  className="forecast-image"
+                ></img>
+                <div className="forecast-temp">
+                  {celcius && (
+                    <p className="ft-left">
+                      {Math.round(
+                        forecast.forecast.forecastday[i].day.mintemp_c
+                      )}
+                      °
+                    </p>
+                  )}
+                  {!celcius && (
+                    <p className="ft-left">
+                      {Math.round(
+                        (forecast.forecast.forecastday[i].day.mintemp_c * 9) / 5
+                      ) + 32}
+                      °
+                    </p>
+                  )}
 
-            <div className="forecast-box">
-              <p className="weekDay"> { dayOfWeek( new Date(forecast.forecast.forecastday[1].date_epoch * 1000).getDay() ) } </p>
-              <img src={forecast.forecast.forecastday[1].day.condition.icon} className="forecast-image"></img>  
-              <div className="forecast-temp">
-                <p className="ft-left">{Math.round(forecast.forecast.forecastday[1].day.mintemp_c)}°</p> 
-                <p  className="ft-right">{Math.round(forecast.forecast.forecastday[1].day.maxtemp_c)}°</p>
+                  {celcius && (
+                    <p className="ft-right">
+                      {Math.round(
+                        forecast.forecast.forecastday[i].day.maxtemp_c
+                      )}
+                      °
+                    </p>
+                  )}
+                  {!celcius && (
+                    <p className="ft-right">
+                      {Math.round(
+                        (forecast.forecast.forecastday[i].day.maxtemp_c * 9) / 5
+                      ) + 32}
+                      °
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-
-            <div className="forecast-box">
-              <p className="weekDay"> { dayOfWeek( new Date(forecast.forecast.forecastday[2].date_epoch * 1000).getDay() ) } </p>
-              <img src={forecast.forecast.forecastday[2].day.condition.icon} className="forecast-image"></img>  
-              <div className="forecast-temp">
-                <p className="ft-left">{Math.round(forecast.forecast.forecastday[2].day.mintemp_c)}°</p> 
-                <p  className="ft-right">{Math.round(forecast.forecast.forecastday[2].day.maxtemp_c)}°</p>
-              </div>
-            </div>
-
-            <div className="forecast-box">
-              <p className="weekDay"> { dayOfWeek( new Date(forecast.forecast.forecastday[3].date_epoch * 1000).getDay() ) } </p>
-              <img src={forecast.forecast.forecastday[3].day.condition.icon} className="forecast-image"></img>  
-              <div className="forecast-temp">
-                <p className="ft-left">{Math.round(forecast.forecast.forecastday[3].day.mintemp_c)}°</p> 
-                <p  className="ft-right">{Math.round(forecast.forecast.forecastday[3].day.maxtemp_c)}°</p>
-              </div>
-            </div>
-
-            <div className="forecast-box">
-              <p className="weekDay"> { dayOfWeek( new Date(forecast.forecast.forecastday[4].date_epoch * 1000).getDay() ) } </p>
-              <img src={forecast.forecast.forecastday[4].day.condition.icon} className="forecast-image"></img>  
-              <div className="forecast-temp">
-                <p className="ft-left">{Math.round(forecast.forecast.forecastday[4].day.mintemp_c)}°</p> 
-                <p  className="ft-right">{Math.round(forecast.forecast.forecastday[4].day.maxtemp_c)}°</p>
-              </div>
-            </div>
-
+            ))}
           </div>
 
           <div className="hourly">
-
             <div className="h-header">
               <h4>Hourly Forecast</h4>
-              <h5> {dayOfWeek(new Date(forecast.forecast.forecastday[0].date_epoch * 1000).getDay())}, { forecast.forecast.forecastday[0].date.slice(8,) } May { forecast.forecast.forecastday[0].date.slice(0,4) }  
-               </h5>
-
+              <h5>
+                {" "}
+                {dayOfWeek(
+                  new Date(
+                    forecast.forecast.forecastday[0].date_epoch * 1000
+                  ).getDay()
+                )}
+                , {forecast.forecast.forecastday[0].date.slice(8)}{" "}
+                {getMonth(
+                  parseInt(
+                    forecast.forecast.forecastday[0].date
+                      .slice(5, 7, 10)
+                      .toString()
+                  )
+                )}{" "}
+                {forecast.forecast.forecastday[0].date.slice(0, 4)}
+              </h5>
             </div>
-            <div className="hourly-inner">
+            <div>
+              {[0, 3, 6, 9, 12, 15, 18, 21].map((h) => (
+                <div className="hourly-inner" key={h}>
+                  <p className="htime">
+                    {forecast.forecast.forecastday[0].hour[h].time.slice(11)}
+                  </p>
+                  <img
+                    src={
+                      forecast.forecast.forecastday[0].hour[h].condition.icon
+                    }
+                    alt="weather"
+                    className="hi"
+                  ></img>
+                  {celcius && (
+                    <p className="htemp">
+                      {Math.round(
+                        forecast.forecast.forecastday[0].hour[h].temp_c
+                      )}
+                      °
+                    </p>
+                  )}
+                  {!celcius && (
+                    <p className="htemp">
+                      {Math.round(
+                        (forecast.forecast.forecastday[0].hour[h].temp_c * 9) /
+                          5
+                      ) + 32}
+                      °
+                    </p>
+                  )}
 
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[0].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[0].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[0].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[0].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[0].wind_dir }</p>  
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[3].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[3].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[3].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[3].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[3].wind_dir }</p>
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[6].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[6].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[6].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[6].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[6].wind_dir }</p>
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[9].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[9].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[9].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[9].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[9].wind_dir }</p>
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[12].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[12].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[12].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[12].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[12].wind_dir }</p>
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[15].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[15].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[15].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[15].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[15].wind_dir }</p>
- 
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[18].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[18].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[18].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[18].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[18].wind_dir }</p>
-
-                <p className="htime">{ forecast.forecast.forecastday[0].hour[21].time.slice(11,) }</p>
-                <img src={ forecast.forecast.forecastday[0].hour[21].condition.icon } alt="weather" className="hi"></img>
-                <p className="htemp">{ Math.round(forecast.forecast.forecastday[0].hour[21].temp_c) }°</p>
-                <p className="hwind">{ Math.round(forecast.forecast.forecastday[0].hour[21].wind_kph *0.277778 ) } m/s</p>
-                <p className="hwdir">{ forecast.forecast.forecastday[0].hour[21].wind_dir }</p>
-
+                  <p className="hwind">
+                    {Math.round(
+                      forecast.forecast.forecastday[0].hour[h].wind_kph *
+                        0.277778
+                    )}{" "}
+                    m/s
+                  </p>
+                  <p className="hwdir">
+                    {forecast.forecast.forecastday[0].hour[h].wind_dir}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-
         </article>
       )}
     </div>
